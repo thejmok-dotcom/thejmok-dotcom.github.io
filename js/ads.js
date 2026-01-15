fc3115e8f4c7a6318ac0f7e274d4368b7eca399f
thejmok-dotcom <thejmok@gmail.com>
2026-01-15 17:17:05 +0900

    Update ads.js

--- js/ads.js
+++ js/ads.js
@@ -1,8 +1,1 @@
-// Google AdSense loader
-(function () {
-  const s = document.createElement('script');
-  s.async = true;
-  s.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7009230262375984';
-  s.crossOrigin = 'anonymous';
-  document.head.appendChild(s);
-})();
+
